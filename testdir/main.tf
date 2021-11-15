6152c5db3ea799aa4436fa49d66c45d54e6db607
davops <davina@litmus.com>
2021-11-15 16:02:51 -0500

    Initial add of dir

---
+++ testdir/main.tf
@@ -0,0 +1,17 @@
+locals {
+  region = "us-west-2"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = local.region
+}
